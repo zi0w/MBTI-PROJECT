62fdc9a48089b3660c3ae78723b8c31e5ec54c77
zi0w <zzz13588@naver.com>
2024-11-28 00:03:26 +0900

    fix: Signup.jsx

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Profile from "../pages/Profile";
-import Signup from "../pages/SignUp";
+import Signup from "../pages/Signup";
 import Test from "../pages/Test";
 import TestResults from "../pages/TestResults";
 import Header from "../components/common/Header";
