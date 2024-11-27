b2cde8450badb019c8a54418a989636addf863eb
zi0w <zzz13588@naver.com>
2024-11-28 00:57:01 +0900

    배포 시도 3

--- src/api/testResult.js
+++ src/api/testResult.js
@@ -2,7 +2,7 @@ import { jsonApi } from "./axios";
 
 export const getTestResults = async () => {
   try {
-    const { data } = await jsonApi.get("/");
+    const { data } = await jsonApi.get("/testResults");
     return data;
   } catch (error) {
     console.error(error);
@@ -12,7 +12,7 @@ export const getTestResults = async () => {
 export const createTestResult = async (resultData) => {
   // 닉네임, 타임스탬프, 엠비티아이, 해석
   try {
-    const { data } = await jsonApi.post("/", resultData);
+    const { data } = await jsonApi.post("/testResults", resultData);
     return data;
   } catch (error) {
     console.error(error);
@@ -20,12 +20,12 @@ export const createTestResult = async (resultData) => {
 };
 
 export const deleteTestResult = async (id) => {
-  const response = await jsonApi.delete(`/${id}`);
+  const response = await jsonApi.delete(`/testResults/${id}`);
   return response.data;
 };
 
 export const updateTestResultVisibility = async ({ id, visibility }) => {
-  const response = await jsonApi.patch(`/${id}`, {
+  const response = await jsonApi.patch(`/testResults/${id}`, {
     visibility,
   });
   return response.data;
