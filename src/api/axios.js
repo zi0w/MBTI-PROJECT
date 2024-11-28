52e3aa5e8dcc5bb5cc903a4f55d724e94017e889
zi0w <zzz13588@naver.com>
2024-11-28 11:15:27 +0900

    update: Implement logout functionality on token expiration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import store from "../redux/config/configStore";
+import { changeLogout } from "../redux/slices/authSlice";
 
 export const authApi = axios.create({
   baseURL: "https://moneyfulpublicpolicy.co.kr",
@@ -7,3 +9,55 @@ export const authApi = axios.create({
 export const jsonApi = axios.create({
   baseURL: "https://assorted-tall-individual.glitch.me",
 });
+
+authApi.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (err) => {
+    alert(err.response.data.message);
+    if (
+      err.response.data.message ===
+      "토큰이 만료되었습니다. 다시 로그인 해주세요."
+    ) {
+      // 로그아웃처리
+
+      store.dispatch(changeLogout());
+      // return useAuthStore.getState().clearAuth();
+    }
+    return Promise.reject(err);
+  }
+);
+authApi.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response;
+  },
+  (err) => {
+    console.log(err);
+    alert(err.response.data.message);
+    if (
+      err.response.data.message ===
+      "토큰이 만료되었습니다. 다시 로그인 해주세요."
+    ) {
+      // 로그아웃처리
+      store.dispatch(changeLogout());
+      // return useAuthStore.getState().clearAuth();
+    }
+    return Promise.reject(err);
+  }
+);
+jsonApi.interceptors.request.use(
+  async (config) => {
+    const { data } = await authApi.get("/user");
+    if (data?.success) return config;
+    return Promise.reject(new Error("사용자 정보 조회에 실패 했습니다."));
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
