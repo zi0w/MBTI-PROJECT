61ac7a9cf49517c443c8c2aa2c445aca45be6a5d
zi0w <zzz13588@naver.com>
2024-11-28 11:23:19 +0900

    update: Sort test results by timestamp in descending order

--- src/pages/TestResults.jsx
+++ src/pages/TestResults.jsx
@@ -10,12 +10,15 @@ const TestResults = () => {
     queryFn: getTestResults,
   });
 
+  const sortedData = data?.slice().sort((a, b) => {
+    return b.timestamp - a.timestamp;
+  });
   // 공개 글 + 내 글만 볼 수 있게 맵
   // 내 글인지 아닌지 먼저 판별(무조건 맵) -> 아닌 건(visibility 판별)
   return (
     <div className="flex flex-col gap-10 items-center mt-10 bg-[#A5FF5E]">
       <h1 className="text-3xl font-semibold">모든 테스트 결과</h1>
-      {data?.map((result) =>
+      {sortedData?.map((result) =>
         result.visibility === true || result.userId === userId ? (
           <TestResultItem key={result.id} result={result} />
         ) : null
